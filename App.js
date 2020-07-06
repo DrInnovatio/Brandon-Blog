1b887e075fede3844a85cc6a270a9ac0a6704b55
DrInnovatio <turing1723@gmail.com>
2020-07-06 21:41:09 +1000

    added a Trash can icon

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/indexScreen';
 import { Provider } from './src/context/BlogContext';
 
-
 const navigator = createStackNavigator({
   Index: IndexScreen
 }, {
