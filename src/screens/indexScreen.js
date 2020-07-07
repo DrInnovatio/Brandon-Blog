32f1593e85a69122b8c7b94033a0e4f38a54dd2a
DrInnovatio <turing1723@gmail.com>
2020-07-07 16:04:52 +1000

    Deleting posts

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
@@ -17,8 +17,10 @@ const IndexScreen = () => {
         renderItem={ ({ item }) => {
           return (
             <View style={ styles.row }>
-              <Text style={ styles.title }>{ item.title }</Text>
-              <Feather style={ styles.icon } name="trash" />
+              <Text style={ styles.title }>{ item.title } - { item.id }</Text>
+              <TouchableOpacity onPress={ () => console.log(item.id) }>
+                <Feather style={ styles.icon } name="trash" />
+              </TouchableOpacity>
             </View>
           )
         } }
@@ -31,7 +33,10 @@ const styles = StyleSheet.create({
   row: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    paddingVertical: 20
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: 'gray'
   },
   title: {
     fontSize: 18
