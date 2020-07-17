0d9ee96efc363fb91352307d38d18c17a1f34efb
DrInnovatio <turing1723@gmail.com>
2020-07-17 20:24:44 +1000

    Navigating backward

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,6 +2,12 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'edit_blogpost':
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id
+          ? action.payload
+          : blogPost;
+      });
     case 'delete_blogpost':
       return state.filter((blogPost) => blogPost.id !== action.payload);
     case 'add_blogpost':
@@ -20,22 +26,28 @@ const blogReducer = (state, action) => {
 const addBlogPost = (dispatch) => {
   return (title, content, callback) => {
     dispatch({ type: 'add_blogpost', payload: { title, content } });
-    callback();
+    if (callback) {
+      callback();
+    }
   };
 };
 
 const deleteBlogPost = (dispatch) => {
   return (id) => {
     dispatch({ type: 'delete_blogpost', payload: id });
+
   };
 };
 
 const editBlogPost = dispatch => {
-  return (id, title, content) => {
+  return (id, title, content, callback) => {
     dispatch({
       type: 'edit_blogpost',
       payload: { id, title, content }
     });
+    if (callback) {
+      callback();
+    }
   };
 };
 
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -4,6 +4,7 @@ import { Context } from '../context/BlogContext';
 import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({ navigation }) => {
+
   const id = navigation.getParam('id');
   const { state, editBlogPost } = useContext(Context);
 
@@ -15,7 +16,7 @@ const EditScreen = ({ navigation }) => {
     <BlogPostForm
       initialValue={ { title: blogPost.title, content: blogPost.content } }
       onSubmit={ (title, content) => {
-        editBlogPost(id, title, content);
+        editBlogPost(id, title, content, () => navigation.pop());
       } }
     />
   );
